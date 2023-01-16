a8ac6af900c26714d1df0a447402ad79ffab0ee9
Jorge Vasconcelos <jorge.r.p.v@hotmail.com>
2023-01-15 21:10:23 -0300

    feat: add args to improve performance

--- main.py
+++ main.py
@@ -1,35 +1,30 @@
-from typing import Optional
-
-import os
 import subprocess
+from datetime import datetime
 
 
-def parse_terminal_output(output: bytes | str, decode: str = 'utf-8'):
-    if isinstance(output, bytes):
-        output = output.decode(decode)
-
+def parse_terminal_output(output: str) -> list[str]:
     output = output.split('\n')
     output = [v for v in output if not v == '']
-
     return output
 
 
-def get_lines_and_values(lines:list[str]) -> dict:
+def get_lines_and_values(lines: list[str]) -> dict[int:str]:
     result = {}
 
     for line in lines:
         colon_index = line.index(":")
 
         line_number = int(line[:colon_index])
-        line_value = line[colon_index +1:]
+        line_value = line[colon_index + 1:]
 
         result[line_number] = line_value
 
     return result
 
-def run_command(search_text:str, file_path:str) -> str:
+
+def run_command(search_text: str, file_path: str) -> str:
     command = "ag"
-    args = ["--numbers", search_text, file_path]
+    args = ["--numbers", '--nocolor', "--nobreak", "--nogroup", search_text, file_path]
     shell_command = [command, *args]
 
     output = subprocess.run(
@@ -37,16 +32,20 @@ def run_command(search_text:str, file_path:str) -> str:
         stdout=subprocess.PIPE,
         text=True
     )
-
     return output.stdout
 
+
 def main():
-    command_output = run_command(search_text='ola',file_path='test.txt')
+    start_time = datetime.now()
 
+    command_output = run_command(search_text='ola', file_path='test.txt')
     response = parse_terminal_output(output=command_output)
-
     matched_texts = get_lines_and_values(lines=response)
+
+    end_time = datetime.now()
+
     print(matched_texts)
+    print(f'Execution duration: {end_time - start_time}')
 
 
 if __name__ == '__main__':
